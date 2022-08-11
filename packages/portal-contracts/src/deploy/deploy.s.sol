60eb0ba0f9f64c089b09ee9cb03fd5f8e4913816
DC <dcposch@dcpos.ch>
2022-08-11 17:27:15 -0400

    redeploy Portal

--- packages/portal-contracts/src/deploy/deploy.s.sol
+++ packages/portal-contracts/src/deploy/deploy.s.sol
@@ -10,36 +10,35 @@ contract PortalDeploy is Script {
     function run(
         bool mainnet,
         ERC20 token,
-        BtcMirror existingMirror
+        BtcTxVerifier existingVerifier
     ) external {
         vm.startBroadcast();
 
         // DEPLOY MIRROR
-        BtcMirror mirror;
-        if (address(existingMirror) != address(0)) {
-            mirror = existingMirror;
+        BtcTxVerifier verifier;
+        if (address(existingVerifier) != address(0)) {
+            verifier = existingVerifier;
         } else if (mainnet) {
             // ...STARTING AT MAINNET BLOCK 739000
-            mirror = new BtcMirror(
+            BtcMirror mirror = new BtcMirror(
                 739000,
                 hex"00000000000000000001059a330a05e66e4fa2d1a5adcd56d1bfefc5c114195d",
                 1654182075,
                 0x96A200000000000000000000000000000000000000000,
                 false
             );
+            verifier = new BtcTxVerifier(mirror);
         } else {
             // ...STARTING AT TESTNET BLOCK 2315360
-            mirror = new BtcMirror(
+            BtcMirror mirror = new BtcMirror(
                 2315360,
                 hex"0000000000000022201eee4f82ca053dfbc50d91e76e9cbff671699646d0982c",
                 1659901500,
                 0x000000000000003723C000000000000000000000000000000000000000000000,
                 true
             );
+            verifier = new BtcTxVerifier(mirror);
         }
-
-        // DEPLOY VERIFIER
-        BtcTxVerifier verifier = new BtcTxVerifier(mirror);
 
         // DEPLOY PORTAL
         new Portal(token, 5, verifier);
--- packages/portal-web/src/utils/constants.ts
+++ packages/portal-web/src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const contractAddrs = {
-  portal: "0xa909c8b6ed96899dfb82a698fef380e8836e00b9",
-  btcTxVerifier: "0xde90dad29f978c5b2108a8554f6b7326db9c1c7c",
+  portal: "0xc4dfb1ae03aaa7d7fa8b28b15b0a50272937bf53",
+  btcTxVerifier: "0xb011e0ff7d952b0b2bc16ef35c899ccc146f4391",
   wbtc: "0xbde8bb00a7ef67007a96945b3a3621177b615c44",
 };
